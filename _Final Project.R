c827b5f952fa216282c695f7dfb9eac2ea5cf86d
rkwon17 <rachelsunkwon@gmail.com>
2019-04-23 09:41:43 -0400

    test white alone variables
    having issues with 2010!

--- _Final Project.R
+++ _Final Project.R
@@ -81,11 +81,19 @@ cendat1990$test <- rowSums(cendat1990[,8:9])
 
 census_dat %<>% dplyr::rename('countyfips' = 'GEOID', 'county' = 'NAME', 'foreign_total' = 'B05006_001E', 'recent_total' = 'B05007_002E',
                               'population' = 'summary_est')
-
+#rachel testing 1990 white only data
+test_vars1990 <- c('P0070001')
+test_cendat1990 <- get_decennial(geography = "tract", variables = test_vars1990, year = 1990, geometry = TRUE, output = 'wide', shift_geo = FALSE, summary_var = 'P0010001', state= 'Massachusetts', county = '01')
 
 # Erin testing 2000 white-only data 
 test_vars2000 <- c('P003003', 'P003004', 'P003001')
 test_cendat2000 <- get_decennial(geography = "tract", variables = test_vars2000, year = 2000, geometry = TRUE, output = 'wide', shift_geo = FALSE, summary_var = 'P001001', state= 'Massachusetts', county = '01')
+
+#rachel testing 2010 white-only data - HI THIS DOESN'T WORK IDK WHY SOS
+test_vars2010 <- c('P0010003','P0010001') #white alone, population total
+test_cendat2010 <- get_decennial(geography = "tract", variables = test_vars2010, year = 2010, geometry = TRUE, output = 'wide', shift_geo = FALSE, summary_var = 'P001001', state= 'Massachusetts', county = 'Suffolk')
+
+
 
 
 m90 <- get_decennial(geography = "state", variables = "H043A001", year = 1990)
